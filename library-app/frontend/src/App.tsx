c8247c36cc004f97c82b18ed4e806697bda1fb44
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2023-04-23 18:30:34 +0900

    implement top search component

--- library-app/frontend/src/App.tsx
+++ library-app/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import Navbar from './layouts/navbar/Navbar'
+import ExploreTopBooks from './layouts/home/ExploreTopBooks.tsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <ExploreTopBooks />
     </>
   )
 }
